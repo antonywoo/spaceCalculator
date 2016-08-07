aab182e128f10e46017a038f1f1b5664f5b46001
Cex <cex@cexs-macmini31.home>
2016-08-07 12:56:56 +0100

    first draft

--- calctor/ViewController.swift
+++ calctor/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import AVFoundation //au
 
 class ViewController: UIViewController {
 
+    enum Operation: String {
+        case Divide = "/"
+        case Plus = "+"
+        case Empty = "empty"
+    }
+    
+    var currentOp: Operation = Operation.Empty
+    
+    @IBOutlet weak var outputLb: UILabel!
+    
+    var runNum = ""
+    var leftVal = ""
+    var rightVal = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func numberPress(btn: UIButton) {
+    print("hi")
+        
+        runNum += "\(btn.tag)"
+        outputLb.text = runNum
     }
+    
+   
 
-
+    @IBAction func divpress(sender: AnyObject) {
+        processOp(Operation.Divide)
+        
+    }
+    
+    @IBAction func plusPress(sender: AnyObject) {
+        processOp(Operation.Plus)
+    }
+    
+    
+    @IBAction func equalPress(sender: AnyObject) {
+        processOp(currentOp)
+    }
+    
+    func processOp(op: Operation) {
+        
+        if currentOp != Operation.Empty {
+            //math
+            if runNum != "" {
+            rightVal = runNum
+            runNum = ""
+            
+            if currentOp == Operation.Divide {
+                result = "\(Double(leftVal)! / Double(rightVal)!)"
+            } else if currentOp == Operation.Plus {
+                result = "\(Double(leftVal)! + Double(rightVal)!)"
+            }
+            
+            leftVal = result
+            outputLb.text = result
+            }
+            currentOp = op
+        } else {
+            leftVal = runNum
+            runNum = ""         //clear screen
+            currentOp = op      // store number
+            
+        }
+        
+    }
+    
 }
 
